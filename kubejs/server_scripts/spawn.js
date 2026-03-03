1cfd842cdbfa944ae634961abba6eef0de98eb93
mockgeek <mockgeek.mabi@gmail.com>
2026-03-03 22:01:04 +0900

    設定見直し

--- kubejs/server_scripts/spawn.js
+++ kubejs/server_scripts/spawn.js
@@ -1,8 +1,7 @@
-// === 設定 ===
-let lastDeathTick = -999999; // 最後に死亡したtickを記録
-const COOLDOWN_TICKS = 1 * 60 * 20; // 15分 = 18000tick
+// ====== 設定 ======
+const COOLDOWN_TICKS = 2 * 60 * 20; // 15分 = 18000tick
 
-// === ボス情報構造体 ===
+// ====== ボス情報構造体 ======
 const bossinfos = [
   {
     bossid: "binah:binah_v_2",
@@ -11,27 +10,33 @@ const bossinfos = [
     mobinfos: [
       {
         mobid: "binah:white_cube",
-        count: 4,
+        spawnCount: 4,
+        maxCount: 15,
       },
       {
         mobid: "binah:black_cube",
-        count: 4,
+        spawnCount: 4,
+        maxCount: 15,
       },
       {
         mobid: "binah:yallow_cube",
-        count: 4,
+        spawnCount: 4,
+        maxCount: 15,
       },
       {
         mobid: "inah:droid",
-        count: 2,
+        spawnCount: 1,
+        maxCount: 4,
       },
       {
         mobid: "binah:drone_missile",
-        count: 2,
+        spawnCount: 1,
+        maxCount: 8,
       },
       {
         mobid: "binah:goliath",
-        count: 1,
+        spawnCount: 1,
+        maxCount: 2,
       },
     ],
   },
@@ -44,66 +49,202 @@ const bossinfos = [
   //   mobinfos: [
   //    {
   //      mobid: "example:mob",
-  //      count: 4,
+  //      spawnCount: 1,
+  //      maxCount: 2
   //    }
   // }
 ];
 
-// ボス召喚
-function spawnBoss(event, nowtick) {
-  debug(`[spawnBoss] at tick ${nowtick}`);
+// ====== 召喚座標のランダム決定(プレイヤー座標基準) ======
+function getRundomNum(min, max, signFlag) {
+  if (signFlag) {
+    if (Math.floor(Math.random() * 2) === 0) {
+      return Math.random() * (max - min + 1) + min;
+    } else {
+      return 0 - (Math.random() * (max - min + 1) + min);
+    }
+  } else {
+    return Math.random() * (max - min + 1) + min;
+  }
+}
 
-  // === bossinfos をループ ===
+// ====== 召喚座標のランダム決定(プレイヤー座標基準) ======
+function getRandomSpawnBlock(players, spawnRangeMin, spawnRangeMax, nowtick) {
+  debug(`[${nowtick}][getRandomSpawnBlock] start`);
+
+  // --- 対象ユーザの選択 ---
+  const indexMin = 0;
+  const indexMax = players.length-1;
+  const index = Math.floor(getRundomNum(indexMin, indexMax, false));
+  const player = players[index];
+  debug(`[${nowtick}][getRandomSpawnBlock] info index:${index} player:${JSON.stringify(player)}`);
+
+  // --- 対象ユーザをベースにXZ座標を乱数取得 ---
+  // --- Y座標はユーザと同じ座標で仮決定 ---
+  let blockX = Math.floor(
+    player.x + getRundomNum(spawnRangeMin, spawnRangeMax, true),
+  );
+  let blockY = Math.floor(player.y);
+  let blockZ = Math.floor(
+    player.z + getRundomNum(spawnRangeMin, spawnRangeMax, true),
+  );
+
+  // --- Y座標のブロックを確認 ---
+  // 空気ブロックの場合、１つ下が空気以外であることを確認
+  // 空気以外の場合、１つ上が空気ブロックであることを確認
+  let playerBlock = {
+    x: Math.floor(player.x),
+    y: Math.floor(player.y),
+    z: Math.floor(player.z),
+  };
+  let resultBlock = { x: blockX, y: blockY, z: blockZ };
+  debug(
+    `[${nowtick}][getRandomSpawnBlock] info playerBlock:${JSON.stringify(playerBlock)} resultBlock:${JSON.stringify(resultBlock)}`,
+  );
+
+  return resultBlock;
+}
+
+// ====== BOSS召喚 ======
+function spawnBoss(event, nowtick, bossinfo) {
+  debug(`[${nowtick}][spawnBoss] start`);
+
+  let level = event.server.getLevel(bossinfo.dim);
+
+  // --- ディメンションに1体のみ ---
+  let existing = level
+    .getEntities()
+    .filter((ent) => ent.type == bossinfo.bossid);
+  debug(`[${nowtick}][spawnBoss] info existing.length=${existing.length}`);
+  if (existing.length > 0) {
+    // スポーンキャンセル
+    return;
+  }
+
+  // --- 死亡後時間経過チェック ---
+  let elapsed = nowtick - bossinfo.lastDeathTick;
+  debug(
+    `[${nowtick}][spawnBoss] info elapsed=${elapsed} COOLDOWN_TICKS:${COOLDOWN_TICKS}`,
+  );
+  if (elapsed < COOLDOWN_TICKS) {
+    // スポーンキャンセル
+    return;
+  }
+
+  // --- 召喚座標取得 ---
+  let playersInDim = event.server.players.filter((p) => {
+    let dimId = String(p.level.dimension);
+    return dimId === bossinfo.dim;
+  });
+  //let pos = { x: 20, y: 70, z: 20 };
+  let pos = getRandomSpawnBlock(playersInDim, 32, 64, nowtick);
+
+  // --- 召喚コマンド実行 ---
+  event.server.runCommand(
+    `execute as @p at @p run summon ${bossinfo.bossid} ${pos.x} ${pos.y} ${pos.z} {PersistenceRequired:1b}`,
+    //`summon ${bossinfo.bossid} ${pos.x} ${pos.y} ${pos.z} {PersistenceRequired:1b}`,
+  );
+  debug(
+    `[${nowtick}][spawnBoss] spawn ${bossinfo.bossid} ${bossinfo.dim} ${JSON.stringify(pos)}`,
+  );
+}
+
+// ====== MOB召喚 ======
+function spawnMob(event, nowtick, bossinfo) {
+  debug(`[${nowtick}][spawnMob] start`);
+
+  let level = event.server.getLevel(bossinfo.dim);
+  let playersInDim = event.server.players.filter((p) => {
+    let dimId = String(p.level.dimension);
+    return dimId === bossinfo.dim;
+  });
+
+  // --- ボスの生存チェック ---
+  let existing = level
+    .getEntities()
+    .filter((ent) => ent.type == bossinfo.bossid);
+  debug(
+    `[${nowtick}][spawnMob] info boss existing.length=${existing.length}`,
+  );
+  if (existing.length < 1) {
+    // スポーンキャンセル
+    return;
+  }
+
+  // --- ディメンションの個体数チェック ---
+  for (let mobinfo of bossinfo.mobinfos) {
+    let mobExisting = level
+      .getEntities()
+      .filter((ent) => ent.type == mobinfo.mobid);
+    debug(
+      `[${nowtick}][spawnMob] info mob:${mobinfo.mobid} existing.length=${mobExisting.length} mobinfo.maxCount=${mobinfo.maxCount}`,
+    );
+
+    // --- MOB数上限チェック ---
+    if (mobExisting.length >= mobinfo.maxCount) {
+      // スポーンキャンセル
+      continue;
+    }
+    //　--- MOB沸き数決定 ---
+    let mobNum = mobinfo.maxCount - mobExisting.length;
+    if (mobNum > mobinfo.spawnCount) {
+      mobNum = mobinfo.spawnCount;
+    }
+    for (let i = 0; i < mobNum; i++) {
+      // --- 召喚座標取得 ---
+      let playersInDim = event.server.players.filter((p) => {
+        let dimId = String(p.level.dimension);
+        return dimId === bossinfo.dim;
+      });
+      let pos = getRandomSpawnBlock(playersInDim, 24, 128, nowtick);
+      // --- 召喚コマンド実行 ---
+      event.server.runCommandSilent(
+        `execute as @p at @p run summon ${mobinfo.mobid} ${pos.x} ${pos.y} ${pos.z}`,
+      );
+      //event.level.spawnEntity(mobinfo.mobid, pos.x, pos.y, pos.z);
+      debug(
+        `[${nowtick}][spawnMob] spawn ${mobinfo.mobid} ${bossinfo.dim} ${JSON.stringify(pos)}`,
+      );
+    }
+  }
+}
+
+// ====== モンスター召喚 ======
+function spawnMonster(event, nowtick) {
+  debug(`[${nowtick}][spawnMonster] start`);
+
+  // --- bossinfos をループ ---
   for (let bossinfo of bossinfos) {
-    debug(`[spawnBoss] boss:${bossinfo.bossid}`);
+    debug(`[${nowtick}][spawnMonster] info boss:${bossinfo.bossid}`);
 
     let level = event.server.getLevel(bossinfo.dim);
 
     // --- ディメンションのロードチェック ---
     if (level == null) {
-      debug(`[spawnBoss] discard level == null`);
+      debug(`[${nowtick}][spawnMonster] discard level == null`);
       continue;
     }
 
     // --- ディメンション内のユーザ数チェック ---
     let playersInDim = event.server.players.filter((p) => {
-      let dimId = String(p.level.dimension); 
+      let dimId = String(p.level.dimension);
       return dimId === bossinfo.dim;
     });
     debug(
-      `[spawnBoss] dim:${bossinfo.dim} playersInDim.length:${playersInDim.length}`,
+      `[${nowtick}][spawnMonster] info dim:${bossinfo.dim} playersInDim.length:${playersInDim.length}`,
     );
     if (playersInDim.length == 0) {
       continue;
     }
 
-    // --- ディメンションに1体のみ ---
-    let existing = level
-      .getEntities()
-      .filter((ent) => ent.type == bossinfo.bossid);
-    if (existing.length > 0) {
-      debug(`[spawnBoss] discard existing.length=${existing.length}`);
-      continue;
-    }
-
-    // --- 死亡後15分経過チェック ---
-    let elapsed = nowtick - lastDeathTick;
-    if (elapsed < COOLDOWN_TICKS) {
-      debug(`[spawnBoss] discard elapsed=${elapsed}`);
-      continue;
-    }
-
-    // --- 召喚コマンド実行 ---
-    let pos = { x: 20, y: 70, z: 20 };
-    event.server.runCommand(
-      `execute as @p at @p run summon ${bossinfo.bossid} ${pos.x} ${pos.y} ${pos.z} {PersistenceRequired:1b}`,
-      //`summon ${bossinfo.bossid} ${pos.x} ${pos.y} ${pos.z} {PersistenceRequired:1b}`,
-    );
-    debug(`[spawnBoss] spawn ${bossinfo.bossid} ${bossinfo.dim} ${nowtick}`);
+    // --- ボス召喚処理 ---
+    spawnBoss(event, nowtick, bossinfo);
+    // --- モブ召喚処理 ---
+    spawnMob(event, nowtick, bossinfo);
   }
 }
 
-// === スポーン設定
+// ====== スポーン設定 ======
 ServerEvents.tick((event) => {
   const nowtick = event.server.getLevel("minecraft:overworld").time;
 
@@ -112,7 +253,7 @@ ServerEvents.tick((event) => {
     return;
   } else {
     debug(
-      `[tick] at tick ${nowtick} server:${event.server} level:"${event.level}`,
+      `[${nowtick}][tick] start server:${event.server} level:"${event.level}`,
     );
   }
 
@@ -120,14 +261,14 @@ ServerEvents.tick((event) => {
   if (nowtick % (1 * 60 * 20) !== 0) {
     return;
   } else {
-    spawnBoss(event, nowtick);
+    spawnMonster(event, nowtick);
   }
 });
 
-// === 死亡イベント
+// ====== 死亡イベント ======
 EntityEvents.death((event) => {
   const nowtick = event.server.getLevel("minecraft:overworld").time;
-  debug(`[death] at tick ${nowtick}`);
+  debug(`[${nowtick}][death] start`);
 
   const e = event.entity;
 
@@ -137,58 +278,9 @@ EntityEvents.death((event) => {
     if (e.level.dimension !== bossinfo.dim) continue;
 
     debug(
-      `[death] ${bossinfo.bossid} died at ${nowtick} tick, the previous death was at ${bossinfo.lastDeathTick}`,
+      `[${nowtick}][death] info ${bossinfo.bossid} died at ${nowtick} tick, the previous death was at ${bossinfo.lastDeathTick}`,
     );
-
+    // ボス死亡時刻の記録
     bossinfo.lastDeathTick = nowtick;
-  }
-});
-
-// === スポーン数制御
-EntityEvents.checkSpawn((event) => {
-  const nowtick = event.server.getLevel("minecraft:overworld").time;
-  debug(`[checkSpawn] at tick ${nowtick}`);
-
-  // --- bossinfos をループ ---
-  for (let bossinfo of bossinfos) {
-    let biome = event.level.getBiome(event.pos).id;
-    let level = event.server.getLevel(bossinfo.dim);
-
-    // --- イベント発生バイオームチェック ---
-    if (biome !== bossinfo.dim) {
-      debug(
-        `[checkSpawn] discard boss:${bossinfo.bossid} biome:${bossinfo.dim} ev_biome:${biome}`,
-      );
-      continue;
-    }
-
-    // --- ディメンションのロードチェック ---
-    if (level == null) {
-      debug(`[checkSpawn] discard level == null`);
-      continue;
-    }
-
-    // --- ボスの生存チェック ---
-    let existing = level
-      .getEntities()
-      .filter((ent) => ent.type == bossinfo.bossid);
-    if (existing.length == 0) {
-      debug(`[checkSpawn] discard boss existing.length=${existing.length}`);
-      continue;
-    }
-
-    // --- ディメンションの個体数チェック ---
-    for (let mobinfo of bossinfo.mobinfos) {
-      let mobExisting = level
-        .getEntities()
-        .filter((ent) => ent.type == mobinfo.mobid);
-      if (mobExisting.length < mobinfo.count) {
-        debug(`[checkSpawn] discard mob existing.length=${mobExisting.length}`);
-        continue;
-      } else {
-        // スポーンキャンセル
-        event.cancel();
-      }
-    }
   }
 });
--- kubejs/server_scripts/teleport.js
+++ kubejs/server_scripts/teleport.js
@@ -1,4 +1,21 @@
-// === 設定 ===
+// ====== テレポート情報構造体 ======
+const teleinfos = [
+  {
+    itemid: "binah:binah_egg",
+    dim: "mypack:fix_desert_1",
+    pos: { x: 0, y: 66, z: 0 }
+  },
+  {
+    itemid: "binah:goliath_vehicle_kit",
+    dim: "mypack:endlesstundra",
+    pos: { x: 0, y: 66, z: 0 }
+  },
+  {
+    itemid: "pomkotsmechs:quest_sheet_01",
+    dim: "mypack:endless_copper",
+    pos: { x: 0, y: 66, z: 0 }
+  },
+];
 const TELEPORT_ITEM = "binah:binah_egg"; // 対象アイテム
 const TELEPORT_DIM = "mypack:endlessdesert"; // 対象ディメンション
 
@@ -6,22 +23,26 @@ ItemEvents.rightClicked((event) => {
   debug(
     `[rightClicked] ${event.player.username} ${event.item.id} ${event.player.level.dimension}`,
   );
-  if (event.item.id == TELEPORT_ITEM) {
-    const player = event.player;
-    const dim = String(player.level.dimension);
 
-    // endlessdesert 以外 → endlessdesert へ転移
-    if (dim !== TELEPORT_DIM) {
+  // --- 右クリックされたアイテムがテレポート対象の場合処理 ---
+  for (let teleinfo of teleinfos) {
+    if (event.item.id == teleinfo.itemid) {
+      const player = event.player;
+      const dim = String(player.level.dimension);
+
+      // endlessdesert 以外 → endlessdesert へ転移
+      if (dim !== teleinfo.dim) {
+        player.server.runCommandSilent(
+          `execute in ${teleinfo.dim} run tp ${player.uuid} ${teleinfo.pos.x} ${teleinfo.pos.y} ${teleinfo.pos.z}`,
+        );
+        return;
+      }
+
+      // endlessdesert → リスポーン地点 へ転移
+      const info = inspectRespawnInfo(player);
       player.server.runCommandSilent(
-        `execute in ${TELEPORT_DIM} run tp ${player.uuid} 0 70 0`,
+        `execute in ${info.spawnDim} run tp ${player.uuid} ${info.spawnPos.getX()} ${info.spawnPos.getY()} ${info.spawnPos.getZ()}`,
       );
-      return;
     }
-
-    // endlessdesert → リスポーン地点 へ転移
-    const info = inspectRespawnInfo(player);
-    player.server.runCommandSilent(
-      `execute in ${info.spawnDim} run tp ${player.uuid} ${info.spawnPos.getX()} ${info.spawnPos.getY()} ${info.spawnPos.getZ()}`,
-    );
   }
 });
